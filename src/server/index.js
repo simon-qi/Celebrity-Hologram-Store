75c76cffa9de7da1f0e2c4ef17d8205eb836690a
Simon Qi <s3qi@uwaterloo.ca>
2019-06-17 03:11:50 -0400

    fix syntax error

--- src/server/index.js
+++ src/server/index.js
@@ -92,7 +92,7 @@ app.delete('/api/delete', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '../../build'));
+  app.use(express.static(path.join(__dirname, '../../build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
